improve angle estimation (weighted average, leverage point detection, ...), confidance interval, residual analysis, bugs fixed

ShockOscillationAnalysis/ShockOscillationAnalysis.py
@@ -4,7 +4,6 @@ Created on Tue Dec 20 09:32:30 2022
 
 @author: Ahmed H. Hanfy
 """
-
 import cv2
 import sys
 import numpy as np
@@ -16,7 +15,7 @@ from .linedrawingfunctions import InclinedLine
 from .generateshocksignal import GenerateShockSignal
 
 px = 1/plt.rcParams['figure.dpi']
-plt.rcParams.update({'font.size': 25})
+plt.rcParams.update({'font.size': 10})
 plt.rcParams["text.usetex"] = True
 plt.rcParams["font.family"] = "serif"
 plt.rcParams['figure.max_open_warning'] = 0
@@ -78,13 +77,12 @@ class BCOLOR:  # For coloring the text in terminal
 
 
 class SOA:
-    def __init__(self, f: int = 1, D: int = 1, 
-                 pixelScale: float = 1, 
-                 univ_unit = {'freq':'fps','dis':'mm', 'angle': 'deg'}):
+    def __init__(self, f: int = 1, D: int = 1, pixelScale: float = 1):
         self.f = f  # ----------------- sampling rate (univ_unit: 'freq')
         self.D = D  # ----------------- refrence distance (univ_unit: 'dis)
         self.pixelScale = pixelScale  # initialize scale of the pixels
-        self.univ_unit = univ_unit  # - universal units used in the analysis
+        # - universal units used in the analysis
+        self.univ_unit = {'freq':'fps','dis':'mm', 'angle': 'deg'}
         self.ClickCount = 0  # -------- initialize the mouse clicks
         self.TempLine = []  # --------- initialize the temporary line array
         self.Temp = cv2.vconcat([])  #- initialize the temporary image
@@ -134,66 +132,61 @@ class SOA:
               to calculate the inclined line and display it on the image.
 
         """
+
+        window_name, img_shape, line_type, line_color = parameters
+        avg = None
+
+        # Unconfirmed line drawing functions
+        def draw_line(start, end, color, thickness=1):
+            # Draws a line on the image.
+            cv2.line(self.Temp, start, end, color, thickness)
+
+
+        def process_line():
+            # Processes the drawn line based on the line type.
+            if line_type == 'V':
+                avg = (self.TempLine[0][0] + self.TempLine[1][0]) // 2
+                draw_line((avg, 0), (avg, img_shape[0]), line_color)
+            elif line_type == 'H':
+                avg = (self.TempLine[0][1] + self.TempLine[1][1]) // 2
+                draw_line((0, avg), (img_shape[1], avg), line_color)
+            elif line_type == 'Inc':
+                avg = InclinedLine(self.TempLine[0], self.TempLine[1], imgShape=img_shape)
+                draw_line(avg[0], avg[1], line_color)
+            return avg
+
         if event == cv2.EVENT_LBUTTONDOWN:
             self.ClickCount += 1
             if len(self.TempLine) == 2:
                 self.line_coordinates = self.TempLine
-            elif len(self.TempLine) == 0: self.TempLine = [(x,y)]
+            elif len(self.TempLine) == 0:
+                self.TempLine = [(x,y)]
 
         # Record ending (x,y) coordintes on left mouse bottom release
         elif event == cv2.EVENT_LBUTTONUP:
             if len(self.TempLine) < 2:
                 self.TempLine.append((x,y))
-
                 # Draw temprary line
-                cv2.line(self.Temp, self.TempLine[0], self.TempLine[1],
-                         (0,0,255), 2)
-                if parameters[2] == 'V':
-                    avg = int((self.TempLine[0][0]+self.TempLine[1][0])/2)
-                    cv2.line(self.Temp, (avg,0), (avg,parameters[1][0]),
-                             CVColor.GREEN, 1)
-                elif parameters[2] == 'H':
-                    avg = int((self.TempLine[0][1]+self.TempLine[1][1])/2)
-                    cv2.line(self.Temp, (0,avg), (parameters[1][1],avg),
-                             CVColor.YELLOW, 1)
-                elif parameters[2] == 'Inc':
-                    P1,P2,m,a = InclinedLine(self.TempLine[0], self.TempLine[1],
-                                             imgShape = parameters[1])
-                    cv2.line(self.Temp, P1, P2, CVColor.BLUE, 1)
+                draw_line(self.TempLine[0], self.TempLine[1], (0, 0, 255), 2)
+                avg = process_line()
+                if avg is not None:
+                    cv2.imshow(window_name, self.Temp)
 
-                cv2.imshow(parameters[0], self.Temp)
             elif self.ClickCount == 2:
 
                 self.Temp = self.clone.copy()
-                cv2.imshow(parameters[0], self.clone)
-                # storing the vertical line
-                if parameters[2] == 'V':
-                    avg = int((self.line_coordinates[0][0]+self.line_coordinates[1][0])/2)
-                    cv2.line(self.Temp, (avg,0), (avg,parameters[1][0]),
-                             CVColor.GREEN, 1)
-
-                # storing the Horizontal line
-                elif parameters[2] == 'H':
-                    avg = int((self.line_coordinates[0][1]+self.line_coordinates[1][1])/2)
-                    cv2.line(self.Temp, (0,avg), (parameters[1][1],avg),
-                             parameters[3], 1)
-
-                elif parameters[2] == 'Inc':
-                    P1, P2, m, a = InclinedLine(self.line_coordinates[0],
-                                                self.line_coordinates[1],
-                                                imgShape = parameters[1])
-                    cv2.line(self.Temp, P1, P2, CVColor.BLUE, 1)
-                    avg = [P1, P2, m, a]
-
-                self.Reference.append(avg)
+                avg = process_line()
+                if avg:
+                    self.Reference.append(avg)
                 self.clone = self.Temp.copy()
-                cv2.imshow(parameters[0], self.clone)
-                print('registered line: {}'.format(avg))
+                cv2.imshow(window_name, self.clone)
+                print(f'Registered line: {avg}')
 
         # Delete draw line before storing
         elif event == cv2.EVENT_RBUTTONDOWN:
             self.TempLine = []
-            if self.ClickCount>0: self.ClickCount -= 1
+            if self.ClickCount>0:
+                self.ClickCount -= 1
             self.Temp = self.clone.copy()
             cv2.imshow(parameters[0], self.Temp)
 
@@ -210,7 +203,8 @@ class SOA:
                 - 'H' for Horizontal line (starts from the left to the right),
                 - 'Inc' for Inclined line (not averaging, takes the exact selected points).
             - **LineNameInd (int)**: Index of the window title from the list.
-            - **Initialize (bool, optional)**: To reset the values of Reference and line_coordinates for a new line set. True or False (Default: False).
+            - **Initialize (bool, optional)**: To reset the values of Reference and
+              line_coordinates for a new line set. True or False (Default: False).
 
         Returns:
             list: Cropping limits or (line set).
@@ -221,7 +215,8 @@ class SOA:
             >>> print(line_set)
 
         .. note::
-            - The function uses the `extract_coordinates` method to interactively draw lines on the image.
+            - The function uses the `extract_coordinates` method to interactively draw lines on the
+              image.
             - It waits until the user presses a key to close the drawing window.
 
         .. note::
@@ -234,9 +229,9 @@ class SOA:
 
         """
 
-        self.clone = img.copy();
-        self.Temp = self.clone.copy();
-        self.TempLine = [];
+        self.clone = img.copy()
+        self.Temp = self.clone.copy()
+        self.TempLine = []
         self.ClickCount = 0
         # Window titles
         WindowHeader = ["First Reference Line (left)",
@@ -248,20 +243,27 @@ class SOA:
             self.line_coordinates = []
         shp = img.shape
         # win_x, win_y = self.screenMidLoc(shp)
-
+        prams = ['new cv window', shp, lineType]
         if   lineType == 'V':
-            prams = [WindowHeader[LineNameInd], shp, lineType]
+            v_draw_color = kwargs.get('v_draw_color', CVColor.GREEN)
+            prams[0] = WindowHeader[LineNameInd]
+            prams.append(v_draw_color)
         elif lineType == 'H':
-            line_color = kwargs.get('line_color', CVColor.YELLOW)
-            prams = [WindowHeader[LineNameInd], shp, lineType, line_color]
+            h_draw_color = kwargs.get('h_draw_color', CVColor.YELLOW)
+            prams[0] = WindowHeader[LineNameInd]
+            prams.append(h_draw_color)
         elif lineType == 'Inc':
-            prams = [WindowHeader[LineNameInd], shp, lineType]
+            inc_draw_color = kwargs.get('inc_draw_color', CVColor.BLUE)
+            prams[0] = WindowHeader[LineNameInd]
+            prams.append(inc_draw_color)
 
         cv2.imshow(WindowHeader[LineNameInd], self.clone)
         cv2.setMouseCallback(WindowHeader[LineNameInd],
                              self.extract_coordinates,prams)
         # Wait until user press some key
-        cv2.waitKey(0); cv2.destroyAllWindows(); cv2.waitKey(1);
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        cv2.waitKey(1)
         return self.Reference
 
     def DefineReferences(self, img: np.ndarray[int], shp: tuple[int],
@@ -295,12 +297,15 @@ class SOA:
             >>> Ref_y0 = -1
             >>> Ref_y1 = -1
             >>> slice_loc = 50
-            >>> ref_x0, ref_y0, ref_y1 = instance.DefineReferences(img, shape, Ref_x0, scale_pixels, Ref_y0, Ref_y1, slice_loc)
+            >>> ref_x0, ref_y0, ref_y1 = instance.DefineReferences(img, shape, Ref_x0,
+                                                                   scale_pixels, Ref_y0,
+                                                                   Ref_y1, slice_loc)
             >>> print(ref_x0, ref_y0, ref_y1)
 
         .. note::
             - The function sets up vertical and horizontal reference lines on the image.
-            - It calculates the pixel scale if `scale_pixels` is True using horizontal distance between the reference vertical lines {Ref_x0}.
+            - It calculates the pixel scale if `scale_pixels` is True using horizontal distance
+              between the reference vertical lines {Ref_x0}.
         """
 
         # Ensure the vertical reference lines are sorted if they are provided
@@ -318,7 +323,7 @@ class SOA:
             Ref_x0.sort()  # to make sure that the limits are properly assigned
         else:
             # set the vertical reference lines if provided
-            self.clone = img.copy();
+            self.clone = img.copy()
             cv2.line(self.clone, (Ref_x0[0], 0), (Ref_x0[0], shp[0]),
                      CVColor.GREEN, 1)
             cv2.line(self.clone, (Ref_x0[1], 0), (Ref_x0[1], shp[0]),
@@ -346,12 +351,10 @@ class SOA:
             elif Ref_y1 != -1:
                 Ref_y0 = Ref_y1+round(slice_loc/self.pixelScale)
             self.Reference.append(Ref_y0)
-            cv2.line(self.clone, (0, Ref_y0), (shp[1], Ref_y0),
-                     CVColor.YELLOW, 1)
+            cv2.line(self.clone, (0, Ref_y0), (shp[1], Ref_y0), CVColor.YELLOW, 1)
         return Ref_x0, Ref_y0, Ref_y1
 
-    def CleanSnapshots(self, img: np.ndarray[int], *args,
-                       **kwargs) -> np.ndarray[int]:
+    def CleanSnapshots(self, img: np.ndarray[int], *args, **kwargs) -> np.ndarray[int]:
         """
         Clean and enhance snapshots based on specified corrections. This method takes an original image snapshot `img` and applies specified corrections based on the provided `*args`.
         Supported corrections include 'Brightness/Contrast', 'Average', and 'FFT'.
@@ -482,17 +485,3 @@ class SOA:
         V_avg = np.mean(dx_dt)  # Calculate the average velocity
         V = dx_dt - V_avg       # Subtract the average velocity from each point
         return V
-
-
-# ------------------------------| Draft code |--------------------------------
-"""
-import screeninfo
-
-def screenMidLoc(self, shp: tuple[int]) -> tuple[int]:
-    screen = screeninfo.get_monitors()[0]
-    screen_width, screen_height = screen.width, screen.height
-    x_pos = (screen_width - shp[1]) // 2
-    y_pos = (screen_height - shp[0]) // 2
-    return x_pos, y_pos
-
-"""

ShockOscillationAnalysis/inc_tracking/inc_tracking.py
@@ -7,29 +7,24 @@ Created on Wed Feb 28 13:47:27 2024
 import cv2
 import sys
 import glob
+import keyboard
 import screeninfo  # .......................... To find the  monitor resolution
 import numpy as np
+from scipy import stats
 import matplotlib.pyplot as plt
 from ..preview import plot_review
 from ..shocktracking import ShockTraking
+from ..decorators import calculate_running_time
 from ..ShockOscillationAnalysis import SOA, CVColor, BCOLOR
 from ..linedrawingfunctions import InclinedLine, AngleFromSlope
 from ..slice_list_generator.list_generation_tools import GenerateIndicesList
-# from preview import plot_review
-# from shocktracking import ShockTraking
-# from ShockOscillationAnalysis import (
-#                                        CVColor, 
-#                                        BCOLOR,
-#                                        SOA,
-#                                       )
-# from linedrawingfunctions import InclinedLine, AngleFromSlope
-# from slice_list_generator.list_generation_tools import GenerateIndicesList
+
 from .inc_tracking_support import (anglesInterpolation, v_least_squares,
-                                   error_sum, shockDomain,
+                                   error_analysis, pop_outlier, shockDomain,
                                    ImportingFiles)
 
 px = 1/plt.rcParams['figure.dpi']
-plt.rcParams.update({'font.size': 25})
+plt.rcParams.update({'font.size': 10})
 plt.rcParams["text.usetex"] = True
 plt.rcParams["font.family"] = "Times New Roman"
 
@@ -90,12 +85,12 @@ class InclinedShockTracking(SOA):
             # If CheckingHeight is a single value, create points based on slice thickness
             Ht = int(CheckingHieght / 2)
             DatumY = VMidPnt - Ht
-            
-        if nPnts < 1:     
-            if CheckingHieght > 10:             
+
+        if nPnts < 1:
+            if CheckingHieght > 10:
                 Pnts = np.linspace(0, CheckingHieght, 10); nPnts = 10
-            elif CheckingHieght > 2 and CheckingHieght <= 10: 
-                Pnts = np.linspace(0,CheckingHieght, CheckingHieght); 
+            elif CheckingHieght > 2 and CheckingHieght <= 10:
+                Pnts = np.linspace(0,CheckingHieght, CheckingHieght);
                 nPnts = CheckingHieght
             else:
                 print(u'\u2717')
@@ -138,7 +133,8 @@ class InclinedShockTracking(SOA):
         print(u'\u2713')
         return slices_info, nPnts, inclinationCheck
 
-    def InclinedShockTracking(self, imgSet: list[np.ndarray],                         # image set for line tracking
+    @calculate_running_time
+    def InclinedShockTracking(self, img_set: list[np.ndarray],                         # image set for line tracking
                               nSlices: int, Ref: list[int], slice_thickness: int = 1, # slices and tracking info.
                               nReview: int|list[int] = 0, output_directory: str = '', # Review parameters
                               **kwargs) -> tuple:                                     # Other parameters
@@ -147,7 +143,7 @@ class InclinedShockTracking(SOA):
         Track and analyze the shock angle in a sequence of images.
 
         Parameters:
-            - **imgSet (list)**: List of images for shock tracking, the images should be formated as numpy array.
+            - **img_set (list)**: List of images for shock tracking, the images should be formated as numpy array.
             - **nSlices (int)**: Number of slices to divide the image into for analysis.
             - **Ref (list)**: Reference points for slices [[x_1, x_2, y], ...].
             - **slice_thickness (int, optional)**: Thickness of each slice. Default is 1.
@@ -177,26 +173,23 @@ class InclinedShockTracking(SOA):
             - It uses least squares method to fit the shock locations and calculates the corresponding angle.
         """
         # Initialize variables for tracking
-        avg_ang_glob= 0          # Global average angle
+        avg_ang_glob = 0         # Global average angle
         count = 0                # Image counter
         midLocs =[]              # List to store mid-locations
         xLocs = []               # List to store all x-location lists of shocks
         avg_slope = 0            # visual average slope [float or list[float]]
-        AvgMidLoc = 0            # Average mid-location
+        avg_midloc = 0           # Average mid-location
         columnY = []             # List to store y-coordinates of the slices
         uncertain_list = []      # List to store uncertain x-locations
         uncertainY_list = []     # List to store y-coordinates of uncertain x-locations
-        shp = imgSet[0].shape    # Shape of images in the image set from the first image
+        shp = img_set[0].shape   # Shape of images in the image set from the first image
         m = []                   # list of slops from least square calculation
-        e = []                   # list of all error sums
         # Optional keyword arguments
         avg_preview_mode = kwargs.get('avg_preview_mode', None)
         review_inc_slice_tracking = kwargs.get('review_inc_slice_tracking', -1)
-        
-        
-
+        img_set_size = len(img_set)
         # Array to review the tracked slices within the iamge set
-        slice_ploting_array = np.zeros(len(imgSet))
+        slice_ploting_array = np.zeros(img_set_size)
         if hasattr(review_inc_slice_tracking, "__len__") and len(review_inc_slice_tracking) == 2:
             review_inc_slice_tracking.sort(); start, end = review_inc_slice_tracking
             try:
@@ -210,16 +203,18 @@ class InclinedShockTracking(SOA):
 
         # Determine half thickness for slice processing
         if slice_thickness > 1: Ht = int(slice_thickness/2)  # Ht -> Half Thickness
-        else: Ht = 1; slice_thickness = 2;
+        else: Ht = 1; slice_thickness = 2
 
         # Initialize upper and lower bounds for slices
-        upper_bounds = np.zeros(nSlices, dtype = int); lower_bounds = np.zeros(nSlices, dtype = int)
+        upper_bounds = np.zeros(nSlices, dtype = int)
+        lower_bounds = np.zeros(nSlices, dtype = int)
 
         for i in range(nSlices):
             upper_bounds[i] = Ref[2][i] - Ht
             lower_bounds[i] = Ref[2][i] + Ht if slice_thickness%2 == 0 else Ref[2][i] + Ht + 1
             columnY.append(Ref[2][i])
         columnY = np.array(columnY)
+        # print(columnY)
 
         # Determine middle index for slices
         midIndx = nSlices // 2
@@ -228,10 +223,10 @@ class InclinedShockTracking(SOA):
         LastShockLoc = -1
 
         xLoc = -1*np.ones(nSlices)
-        AngReg = []
+        shock_deg = []
 
         print('Shock tracking started ...', end=" ")
-        for count, img in enumerate(imgSet):
+        for count, img in enumerate(img_set):
             xLocOld = xLoc.copy()
             xLoc = []; uncertain = []; uncertainY = []
             for i in range(nSlices):
@@ -249,21 +244,14 @@ class InclinedShockTracking(SOA):
             midLocs.append(np.mean(xLoc))
 
             # Calculate the slope using least squares method
-            m.append(v_least_squares(xLoc, columnY, nSlices))
-            AngReg.append(AngleFromSlope(m[-1]))
+            est_shock_slope = v_least_squares(xLoc, columnY, nSlices)
+            shock_deg.append(AngleFromSlope(est_shock_slope))
 
-            # error integeration for each snapshot
-            y_int = y-midLocs[-1]*m[-1]
-            e.append(error_sum(xLoc, columnY, nSlices, m[-1], y_int)/abs(x_i1-x_i2))
-            # if e[-1] < 0.01:
-            #     print(f'error coefficient: {BCOLOR.OKGREEN}{e[-1]*100:0.2f}%{BCOLOR.ENDC} @ {count}')
-            # elif 0.01 <= e[-1] < 0.03:
-            #     print(f'error coefficient: {BCOLOR.WARNING}{e[-1]*100:0.2f}%{BCOLOR.ENDC} @ {count}')
-            # else:
-            #     print(f'error coefficient: {BCOLOR.FAIL}{e[-1]*100:0.2f}%{BCOLOR.ENDC} @ {count}')
+            m.append(est_shock_slope)
 
             xLocs.append(xLoc)
             uncertain_list.append(uncertain); uncertainY_list.append(uncertainY)
+
 
         osc_boundary = kwargs.get('osc_boundary', False)
         if osc_boundary:
@@ -277,11 +265,7 @@ class InclinedShockTracking(SOA):
             mean_min = np.mean(min_b); mean_max = np.mean(max_b)
             kwargs['osc_bound_line_info'] = ([min_b, m_min, mean_min], [max_b, m_max, mean_max])
         print(u'\u2713')
-        if np.mean(e) is not np.nan:
-            print(f'Average error in angle estimation:\t{np.mean(e)*100:0.2f}')
-        else:
-            print(e)
-        
+
         print('Plotting tracked data ...')
         if hasattr(nReview, "__len__"):
             r_range = [0,0,1] # review range
@@ -292,19 +276,113 @@ class InclinedShockTracking(SOA):
             r_range = (0,nReview,1)
             st,en,sp = r_range; n_review = nReview
 
-        AvgMidLoc= np.mean(midLocs)  
-        avg_ang_glob = np.mean(AngReg)
+        avg_midloc= np.mean(midLocs)
+        avg_ang_glob = np.mean(shock_deg)
+        avg_angle_data = np.zeros(4)
+        avg_angle_data[0] = avg_ang_glob
+
+        # Initialize list of residual analysis
+        e = np.zeros(img_set_size, dtype=np.ndarray)
+        pop_ylist = np.zeros(img_set_size, dtype=np.ndarray)
+        Sm = np.zeros(img_set_size)
+
+        # error integeration for each snapshot
+        conf_interval = kwargs.get('conf_interval', 0)
+
+        mid_xloc_conf = 0
+        avg_ang_conf = 0
+        if conf_interval > 0:
+            df = nSlices - 2
+            t = stats.t.ppf(conf_interval, df)
+            # Calculate y statistics
+            y_ss = (columnY-y)**2
+            Sty = np.sum(y_ss)
+            y_dp = y_ss/Sty
+            for n_count, xloc in enumerate(xLocs):
+                y_int = y-midLocs[n_count]*m[n_count]
+                e[n_count], outliers, s = error_analysis(xloc, columnY, nSlices, m[n_count], y_int,
+                                                         t, y_dp, n_count, output_directory)
+
+
+                pop_y = []
+                if len(outliers) > 0:
+                    nxloc = xloc
+                    nyloc = columnY
+                    n_slice_new = nSlices
+
+                    for outlier in outliers:
+                        n_slope, n_midxloc, nxloc, nyloc, popy = pop_outlier(outlier[1],
+                                                                       uncertainY_list[n_count],
+                                                                       nxloc, nyloc, n_slice_new)
+                        if n_slope is not None:
+                            n_slice_new -= 1
+                            for outlier in outliers: outlier[1] = outlier[1]-1
+                            pop_y.append(popy)
+
+                    if n_slope is not None:
+                        # print(f'outlier removed, slope and centroid readjasted @ {n_count}' )
+                        # print(f'old angle was: {shock_deg[n_count]}, old xloc was: {midLocs[n_count]}')
+                        m[n_count] = n_slope
+                        shock_deg[n_count] = AngleFromSlope(n_slope)
+                        midLocs[n_count] = n_midxloc
+                        avg_midloc= np.mean(midLocs)
+                        avg_ang_glob = np.mean(shock_deg)
+                        avg_angle_data[0] = avg_ang_glob
+                        n_y_int = y-n_midxloc*n_slope
+                        n_y_ss = (nyloc-y)**2
+                        n_Sty = np.sum((nyloc - y) ** 2)
+                        n_y_dp = n_y_ss/n_Sty
+                        _,_,s = error_analysis(nxloc, nyloc, n_slice_new,
+                                                              n_slope, n_y_int, t,
+                                                              n_y_dp, n_count, output_directory)
+                Sm[n_count] = s / np.sqrt(Sty)
+                pop_ylist[n_count] = pop_y
+            df = img_set_size - 1
+            t = stats.t.ppf(conf_interval, df)
+            if 0 not in Sm:
+                m_avg = np.sum(m/(Sm**2))/np.sum(1/Sm**2)
+                Sm_avg = np.sqrt(1/np.sum(1/Sm**2))
+                m_conf_int = t * Sm_avg
+                w_avg_ang = AngleFromSlope(m_avg)
+                conf_ang = 180-AngleFromSlope(m_conf_int)
+                print(f'weighted average shock angle: {w_avg_ang:0.2f}\u00B1{conf_ang:0.2f} deg')
+                avg_angle_data[2] = w_avg_ang
+                avg_angle_data[3] = conf_ang
+
+            std_mid_xloc = np.sqrt(np.sum((midLocs-avg_midloc)**2)/df)
+            mid_xloc_conf = t*np.sqrt(std_mid_xloc**2/img_set_size)
+            std_mid_Avg = np.sqrt(np.sum((shock_deg-avg_ang_glob)**2)/df)
+            avg_ang_conf = t*np.sqrt(std_mid_Avg**2/img_set_size)
+            avg_angle_data[1] = avg_ang_conf
+
+            fig, ax = plt.subplots(figsize=(10, 8))
+            ax.hist(shock_deg, bins=20, edgecolor='black')
+            ax.vlines([avg_ang_glob, avg_ang_glob-std_mid_Avg, avg_ang_glob+std_mid_Avg],
+                      0, ax.get_ylim()[1], colors=['tab:red', 'tab:orange', 'tab:orange'],
+                      linestyles=['-', '--', '--'])
+
+            # Add grid, labels, and title
+            ax.grid(True, axis='y', which='major', color='#D8D8D8', linestyle='-', alpha=0.3, lw=1.5)
+
+        print(f'Angle range variation: [{min(shock_deg):0.2f}, {max(shock_deg):0.2f}],', end =' ')
+        print(f'\u03C3 = {np.std(shock_deg):0.2f}')
+        print(f'Average shock loc.: {avg_midloc:0.2f}\u00B1{mid_xloc_conf:0.2f} px')
+        print(f'Average shock angle: {avg_ang_glob:0.2f}\u00B1{avg_ang_conf:0.2f} deg')
+        
+
         if avg_preview_mode != 'avg_ang':
             avg_slope = np.mean(m)*np.ones(n_review)
-            # avg_midLoc = AvgMidLoc*np.ones(n_review)
+            # avg_midLoc = avg_midloc*np.ones(n_review)
             avg_midLoc = midLocs
             avg_ang = avg_ang_glob*np.ones(n_review)
         else:
-            avg_slope = m; avg_midLoc = midLocs; avg_ang = AngReg
+            avg_slope = m; avg_midLoc = midLocs; avg_ang = shock_deg
 
-        if en > len(imgSet):
-            en = len(imgSet)
-            print(f'{BCOLOR.WARNING}Warning: {BCOLOR.ENDC}{BCOLOR.ITALIC}Images to review is out of the image set, only within the range are considered{BCOLOR.ENDC}')
+        if en > img_set_size:
+            en = img_set_size
+            print(f'{BCOLOR.WARNING}Warning: {BCOLOR.ENDC}{BCOLOR.ITALIC}Images to'
+                   ' review is out of the image set, only within the range are'
+                   ' considered{BCOLOR.ENDC}')
 
         if n_review > 20:
              print(f'{BCOLOR.BGOKCYAN}info.:{BCOLOR.ENDC}{BCOLOR.ITALIC} For memory reasons, only 20 images will be displayed.')
@@ -313,11 +391,14 @@ class InclinedShockTracking(SOA):
         if n_review > 0:
             n = 0
             for i in range(st,en,sp):
-                fig, ax = plt.subplots(figsize=(int(shp[1]*1.75*px), int(shp[0]*1.75*px)))
+                fig, ax = plt.subplots(figsize=(int(shp[1]*2.25*px), int(shp[0]*2.25*px)))
                 ax.set_ylim([shp[0],0]); ax.set_xlim([0,shp[1]])
-                plot_review(ax, imgSet[i], shp, xLocs[i], columnY,
+                kwargs['conf_info'] = e[i]
+                kwargs['true_outlier'] = pop_ylist[i]
+                plot_review(ax, img_set[i], shp, xLocs[i], columnY,
                             uncertain_list[i], uncertainY_list[i],
                             avg_slope[i], avg_ang[i], avg_midLoc[i] , y, **kwargs)
+
                 if len(output_directory) > 0:
                     fig.savefig(fr'{output_directory}\ShockAngleReview_Ang{avg_ang_glob:.2f}_{i:05d}.png', bbox_inches='tight', pad_inches=0.1)
 
@@ -332,9 +413,7 @@ class InclinedShockTracking(SOA):
                 sys.stdout.write("[%-20s] %d%%" % ('='*int((n+1)/(n_review/20)), int(5*(n+1)/(n_review/20))))
                 n += 1
             print()
-
-        print(f'Angle range variation: [{min(AngReg):0.2f},{max(AngReg):0.2f}], \u03C3 = {np.std(AngReg):0.2f}')
-        return avg_ang_glob, AvgMidLoc
+        return avg_angle_data, avg_midloc
 
 
     def ShockPointsTracking(self, path: str,
@@ -554,10 +633,15 @@ class InclinedShockTracking(SOA):
             cv2.imshow('investigation domain before rotating', self.clone)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
+            if keyboard.read_key() == "esc":
+                print('Operation not permitted. Terminating process...')
+                sys.exit()
             cv2.waitKey(1)
 
             cv2.imwrite(fr'{output_directory}\AnalysisDomain-Points.jpg',
                         self.clone)
+
+
 
         import_n_files = kwargs.get('n_files', 0)
         if import_n_files == 0:
@@ -581,7 +665,7 @@ class InclinedShockTracking(SOA):
                                                                     output_directory=output_directory,
                                                                     **kwargs)
 
-        print('Average inclination angle {:.2f} deg'.format(avg_shock_angle))
+        # print('Average inclination angle {:.2f} deg'.format(avg_shock_angle))
 
         return avg_shock_angle, avg_shock_loc
 

ShockOscillationAnalysis/inc_tracking/inc_tracking_support.py
@@ -8,11 +8,11 @@ import cv2
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+from ..preview import residual_preview
 from ..linedrawingfunctions import InclinedLine
 from ..ShockOscillationAnalysis import BCOLOR, CVColor
-# from linedrawingfunctions import InclinedLine
-# from ShockOscillationAnalysis import BCOLOR, CVColor
 from scipy.interpolate import CubicSpline, PchipInterpolator
+
 
 def v_least_squares(xLoc: list[float], columnY:list[float], nSlices: int) -> list[float]:
     """
@@ -42,10 +42,11 @@ def v_least_squares(xLoc: list[float], columnY:list[float], nSlices: int) -> lis
     xy = np.array(xLoc)*columnY; yy = columnY**2
     x_sum = np.sum(xLoc)       ; y_sum = np.sum(columnY)
     xy_sum = np.sum(xy)        ; yy_sum = np.sum(yy)
+    if nSlices*yy_sum - y_sum**2 != 0 and nSlices*xy_sum - x_sum * y_sum != 0:
+        return 1/((nSlices*xy_sum - x_sum * y_sum)/(nSlices*yy_sum - y_sum**2))
+    else:
+        return np.inf
 
-    b = 1/((nSlices*xy_sum - x_sum * y_sum)/(nSlices*yy_sum - y_sum**2))
-
-    return b
 
 def pearson_corr_coef(xLoc: list[float], columnY:list[float], nSlices: int) -> list[float]:
 
@@ -90,11 +91,11 @@ def pearson_corr_coef(xLoc: list[float], columnY:list[float], nSlices: int) -> l
     return r
 
 
-def error_sum(xloc: list[float], columnY:list[float], nSlices: int,
-              l_slope: float, l_yint: float):
+def error_analysis(xloc: list[float], columnY:list[float], nSlices: int,
+              l_slope: float, l_yint: float, t: float, y_dp: list[float],
+              count = 0, output_directory= '',**kwargs):
     """
-    Calculate the normalized sum of absolute errors between the actual x-coordinates and
-    the x-coordinates predicted by the linear regression line.
+    Estimate confidence intervals for x-locations based on a linear model and calculate residuals.
 
     Parameters:
         - **xloc (list[float])**: List of actual x-coordinates.
@@ -104,36 +105,109 @@ def error_sum(xloc: list[float], columnY:list[float], nSlices: int,
         - **l_yint (float)**: y-intercept of the linear regression line.
 
     Returns:
-        float: Normalized sum of absolute errors.
+        list of tuple
+            A list of tuples, where each tuple contains:
+            - Predicted x-location (float)
+            - Corresponding 95% confidence interval (float)
+
+    Raises:
+        ValueError
+        If nSlices is less than 3 (as at least 2 degrees of freedom are required).
 
     Example:
-        >>> xloc = [1.0, 2.0, 3.0, 4.0, 5.0]
-        >>> columnY = [2.0, 4.1, 5.9, 8.2, 10.0]
-        >>> nSlices = 5
-        >>> l_slope = 2.0
-        >>> l_yint = 0.0
-        >>> e = error_sum(xloc, columnY, nSlices, l_slope, l_yint)
-        >>> print(e)
+        >>> xloc = [1.2, 2.3, 3.4, 4.5]
+        >>> columnY = [2.1, 3.2, 4.3, 5.4]
+        >>> nSlices = 4
+        >>> l_slope = 1.0
+        >>> l_yint = 1.0
+        >>> conf_est(xloc, columnY, nSlices, l_slope, l_yint)
+        [(1.1, 0.28), (2.1, 0.36), (3.2, 0.45), (4.3, 0.56)]
 
     .. note::
-        - The function calculates the normalized sum of absolute errors by comparing the actual x-coordinates
-          to the x-coordinates predicted by the linear regression line for each y-coordinate.
-        - It returns the average error per slice as a float.
+        This function calculates the residual sum of squares and confidence intervals
+        for the given x-locations based on a linear fit to the corresponding y-values. The
+        confidence interval is computed using the t-distribution for the specified number
+        of slices.
     """
-    error = []
-    e = 0
-    e2 = 0
-    for i in range(nSlices):
-        x_dash = (columnY[i] - l_yint)/l_slope
-        error.append(abs(xloc[i]-x_dash))
-        e += error[-1]
-    e /= nSlices
-    # for i in range(nSlices):
-    #     e2 += (error[i]-e)**2
-    # e2 /= nSlices
-    # print(f'{BCOLOR.OKGREEN}{e:0.2f}\t{BCOLOR.OKCYAN}{e2:0.2f}{BCOLOR.ENDC}')
-    # sigma = np.sqrt(e2)
-    return e
+    # Ensure that the number of slices is sufficient
+    if nSlices < 3:
+        raise ValueError("nSlices must be at least 3 to have enough degrees of freedom.")
+
+    # Convert to NumPy arrays for vectorized operations
+    xloc = np.array(xloc)
+    columnY = np.array(columnY)
+
+    # Calculate predicted x-locations from the linear model
+    x_dash = (columnY - l_yint) / l_slope if l_slope != np.inf else np.ones(nSlices)*np.mean(xloc)
+
+    error = xloc - x_dash
+
+    # Calculate the sum of squared errors
+    Se = np.sum(error ** 2)
+
+    e_median = np.median(error)
+
+    # Split the error array into two parts: below or equal to the median, and above
+    Q1_array, Q2_array = np.array_split(sorted(error), 2)
+
+    Q1 = np.median(Q1_array)
+    Q2 = np.median(Q2_array)
+    IQR = Q2 - Q1
+
+    outlier_p = [[e, i, count] for i, e in enumerate(error)
+                 if not (Q1 - 1.5 * IQR < e < Q2 + 1.5 * IQR)]
+
+    hi = [(1 / nSlices) + y_dp[i] for i, e in enumerate(error)
+          if not (Q1 - 1.5 * IQR < e < Q2 + 1.5 * IQR)]
+
+    if len(hi) > 0 and np.sum(hi) > (3*2)/nSlices:
+        f = open(fr"{output_directory}\outliers.txt", "a")
+        for p in outlier_p:
+            e, pos, count = p
+            f.write(f'outlier detected: {e}, {pos+1}, {count}\n')
+        f.write(f'outlier leverage: {np.sum(hi)}, H0 = {(3*2)/nSlices}\n')
+        f.close()
+        resid_preview = kwargs.get('resid_preview', False)
+        if resid_preview: residual_preview(error, (e_median,Q1,Q2,IQR), nSlices, count)
+    else:
+        outlier_p = []
+
+
+    # Standard error and t-statistics
+    df = nSlices - 2  # ........ Calculate degree of freedom
+    s = np.sqrt(Se / df)
+
+    # Compute confidence intervals for each slice
+    Sx = s * np.sqrt((1 / nSlices) + y_dp)
+
+    # Calculate xloc statistics
+    # xloc_avg = np.mean(xloc)
+    # Stx = np.sum((xloc - xloc_avg) ** 2)
+    # SR = Stx - Se
+
+    # if Stx > 0:
+    #     R2 = SR/Stx
+    #     R2a = 1-((nSlices-1)/Stx)*(s**2)
+    #     print(f'{count}, R²: {R2*100:0.2f}%, Rₐ²: {R2a*100:0.2f}%')
+
+    return list(zip(x_dash, Sx * t)), outlier_p, s
+
+def pop_outlier(indx, certainity, xloc, columnY, n_slice_new):
+    newxloc, newyloc = xloc, columnY
+    try:
+        if len(certainity) > 0 and columnY[indx] in certainity:
+            newxloc = np.delete(xloc, indx)
+            newyloc = np.delete(columnY, indx)
+            new_shock_slope = v_least_squares(newxloc, newyloc, n_slice_new - 1)
+            new_midxloc = np.mean(newxloc)
+            return new_shock_slope, new_midxloc, newxloc, newyloc, [xloc[indx], columnY[indx]]
+
+        # If outlier condition not met
+        return None, None, newxloc, newyloc, None
+    except Exception as e:
+        print(e)
+        return None, None, newxloc, newyloc, None
+
 
 def anglesInterpolation(pnts_y_list: list[int],                              # Generated points by class
                         flow_dir: list[float] = None, flow_Vxy:list[tuple] = None, # measured data (LDA, CFD, ... )

ShockOscillationAnalysis/linedrawingfunctions.py
@@ -83,12 +83,12 @@ def InclinedLine(P1: tuple[int], P2: tuple[int] = (), slope: float = None, imgSh
     elif len(P2) == 0 and slope is np.inf: dx = 0;
     else: dx = -1
 
-    if slope != 0 and slope is not None and slope is not np.inf:
+    if (slope != 0) and (slope is not None) and (slope != np.inf):
         a = P1[1] - slope*P1[0]
         try:
             Xmax = int((imgShape[0]-a)/slope)
         except Exception:
-            print(a,slope)
+            print(a,slope, type(slope))
         Xmin = int(-a/slope)
         if   Xmin >= 0 and Xmin <= imgShape[1]:
             p1 = (Xmin,0)
@@ -102,8 +102,8 @@ def InclinedLine(P1: tuple[int], P2: tuple[int] = (), slope: float = None, imgSh
             p1 = (0,y1)
             p2 = XCheck(Xmax,imgShape,slope,a)
         return p1, p2, slope, a
-    elif dx == 0:
-        return (P1[0],0), (P1[0],imgShape[0]), np.Inf, 0
+    elif dx == 0 or slope == np.inf:
+        return (P1[0],0), (P1[0],imgShape[0]), np.inf, 0
     else:
         return (0,P1[1]), (imgShape[1],P1[1]), 0, P1[1]
 

ShockOscillationAnalysis/preview.py
@@ -7,19 +7,17 @@ Created on Wed Feb 28 11:05:30 2024
 
 import cv2
 import numpy as np
-# from ShockOscillationAnalysis import CVColor
-# from linedrawingfunctions import InclinedLine, AngleFromSlope
+import matplotlib.pyplot as plt
 from .ShockOscillationAnalysis import CVColor
 from .linedrawingfunctions import InclinedLine, AngleFromSlope
 from matplotlib.patches import Arc, FancyArrowPatch
 
-
-
-def AvgAnglePlot(ax, img_shp:tuple , P: tuple, slope: float, angle: float, 
+def AvgAnglePlot(ax, img_shp:tuple , P: tuple, slope: float, angle: float,
                  txt: bool = True, lin_color = 'w', lin_opacity = 1, **kwargs) -> None:
     """
     Plot the average angle line and optional text annotation on a given axis.
-    This function uses the `InclinedLine` function to determine the end points of the line based on the given slope and image shape.
+    This function uses the `InclinedLine` function to determine the end points
+    of the line based on the given slope and image shape.
     It then plots the line and an optional text annotation indicating the angle.
 
     Parameters:
@@ -32,7 +30,7 @@ def AvgAnglePlot(ax, img_shp:tuple , P: tuple, slope: float, angle: float,
         - `**kwargs`: Additional keyword arguments for customization:
             - **avg_txt_Yloc (int, optional)**: Y location for the text annotation. Default is image height minus 100.
             - **avg_txt_size (int, optional)**: Font size of the text annotation. Default is 26.
-    
+
     Example:
         >>> fig, ax = plt.subplots()
         >>> img_shp = (600, 800)
@@ -43,36 +41,39 @@ def AvgAnglePlot(ax, img_shp:tuple , P: tuple, slope: float, angle: float,
         >>> plt.show()
     """
     # Handle optional parameter values from **kwargs
-    
-    
+
+
     avg_txt_Yloc = kwargs.get('avg_txt_Yloc', img_shp[0]-100)
     avg_txt_size = kwargs.get('avg_txt_size', 26)
-    
+
     # Calculate the inclined line end points
     P1,P2,_,a = InclinedLine(P,slope = slope ,imgShape = img_shp)
+
     # Calculate the X position for the text annotation
-    X = int((avg_txt_Yloc-a)/slope) if slope != 0 else avg_txt_Yloc
+    if slope != 0 and slope != np.inf: X = int((avg_txt_Yloc-a)/slope)
+    elif slope == np.inf: X = P[0]
+    else: X = avg_txt_Yloc
+
     # Plot the inclined line
     ax.plot([P1[0],P2[0]], [P1[1],P2[1]], lw = 2,
             color= lin_color, linestyle = (0, (20, 3, 5, 3)), alpha = lin_opacity)
-    
+
     # Plot the text annotation if enabled
     if txt:
         # Draw an arc to represent the angle
         avg_ang_arc = Arc((X, avg_txt_Yloc),80, 80, theta1= -angle , theta2 = 0, color = lin_color)
         ax.add_patch(avg_ang_arc);
-       
+
         # Add the text annotation for the angle
-        ax.text(X + 40 ,avg_txt_Yloc-10 , f'${{{angle:0.2f}}}^\circ$', 
+        ax.text(X + 40 ,avg_txt_Yloc-10 , f'${{{angle:0.2f}}}^\circ$',
                 color = lin_color, fontsize = avg_txt_size);
         # Plot a horizontal line at the text annotation location to compare the inclination angle
-        ax.plot([X-10,X+100], [avg_txt_Yloc,avg_txt_Yloc], lw = 1, 
+        ax.plot([X-10,X+100], [avg_txt_Yloc,avg_txt_Yloc], lw = 1,
                 color = lin_color, alpha = lin_opacity)
 
-
 def plot_review(ax, img, shp, x_loc, column_y, uncertain, uncertain_y, avg_slope, avg_ang,
-                mid_loc = -1, y = -1, avg_preview_mode = None,Mach_ang_mode = None, **kwargs):
-    
+                mid_loc = -1, y = -1, avg_preview_mode = None, Mach_ang_mode = None, **kwargs):
+
     """
     Plot review function to visualize shock points and additional features on an image.
 
@@ -95,36 +96,54 @@ def plot_review(ax, img, shp, x_loc, column_y, uncertain, uncertain_y, avg_slope
     Returns:
         None
     """
+    # tracking_std = kwargs.get('tracking_std', False)
     points_color = kwargs.get('points_color', 'yellow')
     points_opacity = kwargs.get('points_opacity', 1)
+    points_size = kwargs.get('points_size', 12)
     uncertain_point_color = kwargs.get('uncertain_point_color', 'red')
-    # tracking_std = kwargs.get('tracking_std', False)
-    
+
     ax.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB).astype(np.uint8))
-    ax.plot(x_loc, column_y, '-o', color=points_color, ms=12, alpha = points_opacity)
-    ax.plot(uncertain, uncertain_y, 'o', color=uncertain_point_color, ms=12, alpha = points_opacity)
-   
+
     if avg_preview_mode is not None:
         avg_lin_color = kwargs.get('avg_lin_color', 'w')
         avg_show_txt = kwargs.get('avg_show_txt', True)
         avg_lin_opacity = kwargs.get('avg_lin_opacity', 1)
-        AvgAnglePlot(ax, shp, (mid_loc,y), avg_slope, avg_ang, 
+
+        AvgAnglePlot(ax, shp, (mid_loc,y), avg_slope, avg_ang,
                      txt = avg_show_txt, lin_color = avg_lin_color, lin_opacity = avg_lin_opacity,
                      **kwargs)
-    
+
     osc_boundary = kwargs.get('osc_boundary', False)
     if osc_boundary:
         min_bound, max_bound = kwargs.get('osc_bound_line_info', np.zeros(len(column_y)))
         b_color = kwargs.get('b_color', 'tab:orange')
         b_lins_opacity = kwargs.get('b_lins_opacity', 1)
         osc_range_opacity = kwargs.get('osc_range_opacity', 0.3)
-        AvgAnglePlot(ax, shp, (min_bound[2],y), min_bound[1], AngleFromSlope(min_bound[1]), 
+        AvgAnglePlot(ax, shp, (min_bound[2],y), min_bound[1], AngleFromSlope(min_bound[1]),
                      txt= False, lin_color = b_color, lin_opacity = b_lins_opacity)
-        AvgAnglePlot(ax, shp, (max_bound[2],y), max_bound[1], AngleFromSlope(max_bound[1]), 
+        AvgAnglePlot(ax, shp, (max_bound[2],y), max_bound[1], AngleFromSlope(max_bound[1]),
                      txt= False, lin_color = b_color, lin_opacity = b_lins_opacity)
-        
-        # ax.fill_betweenx([column_y[0],column_y[-1]], min_bound[0],max_bound[0])
         ax.fill_betweenx(column_y, min_bound[0],max_bound[0], color = b_color, alpha = osc_range_opacity)
+
+    conf_interval = kwargs.get('conf_interval', 0)
+
+    if conf_interval > 0:
+        conf_info = kwargs.get('conf_info', np.zeros([len(column_y),2]))
+        conf_color = kwargs.get('conf_color', 'tab:green')
+        conf_range_opacity = kwargs.get('conf_range_opacity', 0.3)
+        true_outlier = kwargs.get('true_outlier', None)
+        x_new_loc, conf_value = zip(*conf_info)
+        x_new_loc = np.array(x_new_loc)
+        conf_value = np.array(conf_value)
+        for x, y in enumerate(column_y):
+            ax.plot([x_loc[x],x_new_loc[x]],[y,y],':', color = 'white')
+
+        ax.fill_betweenx(column_y, x_new_loc-conf_value, x_new_loc+conf_value,
+                         color = conf_color, alpha = conf_range_opacity)
+        if len(true_outlier) > 0:
+            for item in true_outlier:
+                ax.plot(item[0],item[1],'x', color = 'green', ms = points_size+2)
+
     if Mach_ang_mode =='Mach_num':
         inflow_dir_deg = kwargs.get('inflow_dir_deg', np.zeros(len(column_y)))
         inflow_dir_rad = kwargs.get('inflow_dir_rad', np.zeros(len(column_y)))
@@ -141,7 +160,7 @@ def plot_review(ax, img, shp, x_loc, column_y, uncertain, uncertain_y, avg_slope
             ax.text(p1[0]+40 ,p1[1]- 5 , f'${{{inflow_ang:0.2f}}}^\circ$',
                     size = M1_txt_size, color = M1_color);
             ax.plot([p1[0]-arw_len*xlen,p1[0]+60*xlen], [p1[1]-arw_len*ylen,p1[1]+60*ylen],color = M1_color, lw = 1)
-            
+
             arc1 = Arc(p1,arc_dia, arc_dia, theta1=-local_ang, theta2=0+inflow_dir_deg[i], color = M1_color)
             ax.add_patch(arc1);
             M1 = 1/np.sin((inflow_ang)*np.pi/180)
@@ -153,7 +172,13 @@ def plot_review(ax, img, shp, x_loc, column_y, uncertain, uncertain_y, avg_slope
                         xytext=(p1[0] - arw_len*xlen, p1[1] + arw_len*ylen),
                         horizontalalignment='right', verticalalignment='center')
 
-def PreviewCVPlots(img, Ref_x0 = [], Ref_y = [], 
+    ax.plot(x_loc, column_y, '-o',
+            color=points_color, ms=points_size, alpha=points_opacity)
+
+    ax.plot(uncertain, uncertain_y, 'o',
+            color=uncertain_point_color, ms=points_size, alpha=points_opacity)
+
+def PreviewCVPlots(img, Ref_x0 = [], Ref_y = [],
                    tk = [], avg_shock_loc = [], **kwargs):
     """
     PreviewCVPlots function is used to overlay various plot elements on an image for visualization purposes.
@@ -175,7 +200,7 @@ def PreviewCVPlots(img, Ref_x0 = [], Ref_y = [],
     Returns:
         **numpy.ndarray**: Image with overlaid plot elements.
     """
-    
+
     shp = img.shape;
     if len(Ref_x0):
         Ref_x0_color = kwargs.get('Ref_x0_color', CVColor.GREEN)
@@ -202,7 +227,30 @@ def PreviewCVPlots(img, Ref_x0 = [], Ref_y = [],
 
     return img
 
+def residual_preview(error, margins, nSlices, count):
+    fig, ax = plt.subplots(figsize=(10, 8))
+    e_median, Q1, Q2, IQR = margins
+    try:
+        # Plot histogram
+        ax.hist(error, bins=20, edgecolor='black')
 
+        # Update bar colors based on outlier range
+        for bar in ax.containers[0]:
+            x = bar.get_x() + 0.5 * bar.get_width()
+            color = 'red' if x < Q1 - 1.5 * IQR or x > Q2 + 1.5 * IQR else 'tab:blue'
+            bar.set_color(color)
+            bar.set_edgecolor('black')
+
+        # Draw vertical lines for median and IQR bounds
+        ax.vlines([e_median, Q1 - 1.5 * IQR, Q2 + 1.5 * IQR], 0, nSlices,
+                  colors=['tab:red', 'tab:orange', 'tab:orange'], linestyles=['-', '--', '--'])
+
+        # Add grid, labels, and title
+        ax.grid(True, axis='y', which='major', color='#D8D8D8', linestyle='-', alpha=0.3, lw=1.5)
+        ax.set_ylim([0, nSlices])
+        ax.set_title(f'Outliers have high impact at {count}')
+    except Exception as e:
+        print(f'Error during plotting: {e}')
 
 # -----------------------------| Draft code |----------------------------------
 # ploting the middle point as possible center of rotation

ShockOscillationAnalysis/shocktracking.py
@@ -44,9 +44,10 @@ def ShockTraking(SnapshotSlice, LastShockLoc = -1, Plot = False, count = -1, Alp
         ax.plot(SnapshotSlice, label = 'Light intensity at certain snapshot'); ax.axhline(avg,linestyle = ':',color = 'tab:green', label = 'Light intensity average line');
         # ax.set_ylim([0,255]);  ax.set_yticks(np.arange(0, 260, 51))
         # ax.plot(np.where(SnapshotSlice == MinimumPoint),MinimumPoint,'xr', label = 'Minimum point of local minimum');
-        ax.axhline(MinimumPoint,linestyle = '--',color = 'k');
+        ax.axhline(MinimumPoint,linestyle = '--',color = 'k')
         ax.set_ylim([-20,255]);  ax.set_yticks(np.arange(0, 260, 51))
-        ax.axhline(0,linestyle = ':', color = 'k', alpha = 0.2); ax.axhline(255,linestyle = ':', color = 'k', alpha = 0.2);
+        ax.axhline(0,linestyle = ':', color = 'k', alpha = 0.2); 
+        ax.axhline(255,linestyle = ':', color = 'k', alpha = 0.2)
         # ax.plot(AvgLocation,AvgIllumination,linestyle = '-.');
     
     # Initiating Variables 
@@ -92,7 +93,8 @@ def ShockTraking(SnapshotSlice, LastShockLoc = -1, Plot = False, count = -1, Alp
     except Exception as e:
         # By this way we can know about the type of error occurring
         fig, ax = plt.subplots(figsize=(10,5))
-        ax.plot(SnapshotSlice); ax.axhline(avg,linestyle = ':');
+        ax.plot(SnapshotSlice)
+        ax.axhline(avg,linestyle = ':')
         ax.text(0.99, 0.99, f'Error: {e}',
                 ha = 'right', va ='top', transform = ax.transAxes,
                 color = 'red', fontsize=14)
@@ -111,7 +113,8 @@ def ShockTraking(SnapshotSlice, LastShockLoc = -1, Plot = False, count = -1, Alp
     for k in range(len(ShockRegion[1])):
         # check all pixels lays under the valley average line
         if ShockRegion[1][k] < LocMinAvg:
-            localmin2.append(ShockRegion[1][k]); LocMinI2.append(ShockRegion[0][k])
+            localmin2.append(ShockRegion[1][k])
+            LocMinI2.append(ShockRegion[0][k])
         elif ShockRegion[1][k] >= LocMinAvg and len(localmin2) > 1:
             SubLocalMinSets.append([LocMinI2,localmin2])
             n += 1; localmin2 = []; LocMinI2 = []
@@ -144,7 +147,7 @@ def ShockTraking(SnapshotSlice, LastShockLoc = -1, Plot = False, count = -1, Alp
                 MinDis = Distance;  ShockRegion = SubLocalMinSet
             if Plot: ax.fill_between(ShockRegion[0], ShockRegion[1],avg , hatch='\\')
     elif n > 1 and LastShockLoc < 0: 
-        n = 1;
+        n = 1
         certainLoc = False
         reason = 'First pexil slice, No shock location history'
     
@@ -157,7 +160,7 @@ def ShockTraking(SnapshotSlice, LastShockLoc = -1, Plot = False, count = -1, Alp
         ax.fill_between(ShockRegion[0], ShockRegion[1],avg, color = '#1F79B7', edgecolor='k',hatch='///', label = 'Largest local minimum') 
         # ax.plot(np.where(SnapshotSlice == min(ShockRegion[1])),min(ShockRegion[1]),'xr');
         
-    shockLoc = [];
+    shockLoc = []
     for elment in range(len(ShockRegion[1])):
         if ShockRegion[1][elment] <= LocMinRMS: shockLoc.append(ShockRegion[0][elment])
     minLoc = np.mean(shockLoc) 
@@ -200,10 +203,11 @@ def ShockTraking(SnapshotSlice, LastShockLoc = -1, Plot = False, count = -1, Alp
                 if MaxArea2 > 0: Ra = Area/MaxArea2
                 if Ra > 0.5 and Ra < 1 and certainLoc: certainLoc = False; reason = 'Almost equal sub-Valleys'   
                 if MaxArea2 !=  abs(np.trapz(LocMinAvg-ShockRegion[1])) and certainLoc: 
-                    certainLoc = False; reason = 'different sub-Valleys than smallest'
+                    certainLoc = False
+                    reason = 'different sub-Valleys than smallest'
         except Exception as e:
             fig, ax = plt.subplots(figsize=(10,5))
-            ax.plot(SnapshotSlice); ax.axhline(avg,linestyle = ':');
+            ax.plot(SnapshotSlice); ax.axhline(avg,linestyle = ':')
             ax.text(0.99, 0.99, f'Error: {e}',
                     ha = 'right', va ='top', transform = ax.transAxes,
                     color = 'red', fontsize=14)

ShockOscillationAnalysis/slice_list_generator/slice_list_generator.py
@@ -7,7 +7,7 @@ Created on Sun Dec  3 16:45:35 2023
 import sys
 import cv2
 import glob
-import math
+import keyboard
 import numpy as np
 from ..ShockOscillationAnalysis import SOA
 from datetime import datetime as dt
@@ -52,8 +52,8 @@ class SliceListGenerator(SOA):
             - The function calculates the intersection point and angles between two lines specified by their slopes and y-intercepts.
             - Returns the intersection point coordinates and angles of the lines in degrees.
         """
-        theta1 = math.degrees(np.arctan(M[0]))
-        theta2 = math.degrees(np.arctan(M[1]))
+        theta1 = np.rad2deg(np.arctan(M[0]))
+        theta2 = np.rad2deg(np.arctan(M[1]))
 
         Xint, Yint = None, None
 
@@ -234,7 +234,8 @@ class SliceListGenerator(SOA):
             sat_vr.sort()
             if abs(end_vr-start_vr) == 0:
                 sat_vr = slice_thickness
-                
+        
+        nPnts = kwargs.get('nPnts', 0)
         if not hasattr(inclination_est_info, "__len__"):
             self.LineDraw(self.clone, 'Inc', 3)
             if len(self.Reference) < 4: print(f'{BCOLOR.FAIL}Error: {BCOLOR.ENDC}{BCOLOR.ITALIC}Reference lines are not sufficient!{BCOLOR.ENDC}'); sys.exit()
@@ -242,7 +243,8 @@ class SliceListGenerator(SOA):
             Ref, nSlices, inclinationCheck = self.inc_trac.InclinedShockDomainSetup(inclination_est_info,
                                                                                     sat_vr, [P1,P2,m,a],
                                                                                     shp, VMidPnt=Ref_y1,
-                                                                                    preview_img=self.clone)
+                                                                                    preview_img=self.clone,
+                                                                                    nPnts=nPnts)
         elif len(inclination_est_info) > 2:
             P1,P2,m,a = InclinedLine(inclination_est_info[1],inclination_est_info[2],imgShape = shp)
             cv2.line(self.clone, P1, P2, CVColor.GREEN, 1)
@@ -250,7 +252,8 @@ class SliceListGenerator(SOA):
             Ref, nSlices, inclinationCheck = self.inc_trac.InclinedShockDomainSetup(inclination_est_info[0],
                                                                                     sat_vr, [P1,P2,m,a],
                                                                                     shp, VMidPnt=Ref_y1,
-                                                                                    preview_img=self.clone)
+                                                                                    preview_img=self.clone,
+                                                                                    nPnts=nPnts)
         elif avg_shock_angle != 90 and avg_shock_loc == 0: # in case the rotation angle only is provieded in working _range
             print(f'{BCOLOR.BGOKGREEN}Request: {BCOLOR.ENDC}{BCOLOR.ITALIC}Please, provide the rotation center...{BCOLOR.ENDC}')
             self.LineDraw(self.clone, 'Inc', 3)
@@ -261,7 +264,12 @@ class SliceListGenerator(SOA):
 
         if preview:
             cv2.imshow('investigation domain before rotating', self.clone)
-            cv2.waitKey(0); cv2.destroyAllWindows(); cv2.waitKey(1)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+            if keyboard.read_key() == "esc":
+                print('Operation not permitted. Terminating process...')
+                sys.exit()
+            cv2.waitKey(1)
 
         # number of files to be imported
         import_n_files = kwargs.get('n_files', 0)
@@ -292,23 +300,28 @@ class SliceListGenerator(SOA):
             else:
                 NSamplingReview = shock_angle_samples
                 print(f'{BCOLOR.WARNING}Warning:{BCOLOR.ENDC}{BCOLOR.ITALIC} Number of samples is larger than requested to review!, all samples will be reviewed{BCOLOR.ENDC}')
-
+                    
             avg_shock_angle, avg_shock_loc = self.inc_trac.InclinedShockTracking(samplesList,
                                                                                  nSlices, Ref,
                                                                                  nReview=NSamplingReview,
-                                                                                 output_directory=output_directory)
-        print('Average inclination angle {:.2f} deg'.format(avg_shock_angle))
-
-        M = cv2.getRotationMatrix2D((avg_shock_angle, Ref_y1), 90-avg_shock_angle, 1.0)
+                                                                                 output_directory=output_directory,
+                                                                                 **kwargs)
+            
+        M = cv2.getRotationMatrix2D((avg_shock_angle[2], Ref_y1), 90-avg_shock_angle[2], 1.0)
         new_img = cv2.warpAffine(img, M, (shp[1],shp[0]))
 
         new_img = PreviewCVPlots(new_img, Ref_x0, Ref_y = Ref_y1,
                                  tk = [lower_bounds,upper_bounds],
                                  avg_shock_loc = avg_shock_loc)
 
-        if avg_shock_angle != 90 and preview:
+        if avg_shock_angle[2] != 90 and preview:
             cv2.imshow('Final investigation domain', new_img)
-            cv2.waitKey(0); cv2.destroyAllWindows(); cv2.waitKey(1)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+            if keyboard.read_key() == "esc":
+                print('Operation not permitted. Terminating process...')
+                sys.exit()
+            cv2.waitKey(1)
 
         if len(output_directory) > 0:
             if len(comment) > 0:
@@ -317,8 +330,8 @@ class SliceListGenerator(SOA):
                 now = dt.now()
                 now = now.strftime("%d%m%Y%H%M")
                 outputPath =f'{output_directory}\\{self.f/1000:.1f}kHz_{slice_loc}mm_{self.pixelScale}mm-px_tk_{slice_thickness}px_{now}'
-            if avg_shock_angle != 90:
-                print('RotatedImage:', u"stored \u2713" if cv2.imwrite(outputPath+f'-RefD{round(avg_shock_angle,2)}deg.png', new_img) else f'{BCOLOR.FAIL}Error: {BCOLOR.ENDC}{BCOLOR.ITALIC}Failed!{BCOLOR.ENDC}')
+            if avg_shock_angle[2] != 90:
+                print('RotatedImage:', u"stored \u2713" if cv2.imwrite(outputPath+f'-RefD{round(avg_shock_angle[2],2)}deg.png', new_img) else f'{BCOLOR.FAIL}Error: {BCOLOR.ENDC}{BCOLOR.ITALIC}Failed!{BCOLOR.ENDC}')
                 print('DomainImage:' , u"stored \u2713" if cv2.imwrite(outputPath+'-RefD.png', self.clone)   else f'{BCOLOR.FAIL}Error: {BCOLOR.ENDC}{BCOLOR.ITALIC}Failed!{BCOLOR.ENDC}')
             else: print('DomainImage:',u"stored \u2713" if cv2.imwrite(outputPath+'-RefD.png', self.clone)   else f'{BCOLOR.FAIL}Error: {BCOLOR.ENDC}{BCOLOR.ITALIC}Failed!{BCOLOR.ENDC}')
 
@@ -338,8 +351,7 @@ class SliceListGenerator(SOA):
         print('working range is: ', working_range)
         print(f'Importing {n_images} images ...')
         img_list, n = self.ImportingFiles(files, indices_list, n_images, shp,
-                                          x_range,
-                                          [upper_bounds, lower_bounds], M)
+                                          x_range, [upper_bounds, lower_bounds], M)
 
         if len(output_directory) > 0:
             print('ImageList write:', f"File was stored: {outputPath}.png" if cv2.imwrite(f'{outputPath}.png', img_list) else f'{BCOLOR.FAIL}Error: {BCOLOR.ENDC}{BCOLOR.ITALIC}Failed!{BCOLOR.ENDC}')